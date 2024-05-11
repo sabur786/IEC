8ca0f2017ccc1124fb95081a569e1c6db26fc600
sabur786 <imtiazrizvi786@yahoo.com>
2024-05-11 11:27:05 -0500

    Update my_yelp.py

--- my_yelp.py
+++ my_yelp.py
@@ -1,13 +1,15 @@
+
+
 import requests, json 
 
 def search_businesses(search_term, search_location):
     
-    api_key = "s_OpTZ_NQ-drrRvI4LZMVU6BCRdxj3QjEMHDFKi5FYMFFEFhlVb2UtnEKnA8d5y-61G1b1Be-94a725Ri739UNcq_IYw-jM3X3UoEssWx8RklEJddu8JTVrikUc2ZnYx"
+    api_key = 's_OpTZ_NQ-drrRvI4LZMVU6BCRdxj3QjEMHDFKi5FYMFFEFhlVb2UtnEKnA8d5y-61G1b1Be-94a725Ri739UNcq_IYw-jM3X3UoEssWx8RklEJddu8JTVrikUc2ZnYx'
 
     url = "https://api.yelp.com/v3/businesses/search"
 
     my_headers = {
-        "Authorization": "Bearer %s" % api_key
+        "Authorization": "Bearer {}".format(api_key)
     }
 
     my_params = {
@@ -18,12 +20,9 @@ def search_businesses(search_term, search_location):
 
     businesses_object = requests.get(url, headers=my_headers, params=my_params)
 
-    businesses_dict = json.loads(businesses_object.text)
-
-    businesses_list = businesses_dict['businesses']
+    businesses_dict = businesses_object.text
     
-    print(businesses_dict)
-    list_of_businesses = []
-    for each in businesses_list:
-        list_of_businesses.append(each)
-    return list_of_businesses
+    #print(businesses_dict)
+
+print(search_businesses("hotels", "miami"))
+print("hello")
